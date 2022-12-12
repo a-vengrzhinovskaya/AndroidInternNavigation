2069832b2c833309ef76d8336106663d72da140b
wdkokskwpqo <a.vengrzhinovskaya@gmail.com>
2022-12-12 14:42:58 +0700

    #45

---
+++ app/src/main/java/com/example/androidinternnavigation/RedFragment.kt
@@ -0,0 +1,38 @@
+package com.example.androidinternnavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.androidinternnavigation.databinding.FragmentRedBinding
+
+class RedFragment : Fragment() {
+    private var _binding: FragmentRedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRedBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navButtonSetOnClick(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun navButtonSetOnClick(view: View) {
+        binding.btRed.setOnClickListener {
+            view.findNavController().navigate(R.id.action_frRed_to_frGreen)
+        }
+    }
+}
