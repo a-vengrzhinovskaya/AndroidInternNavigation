32e4bb3458c80769aaa31408c3adcbaafdc7f847
wdkokskwpqo <a.vengrzhinovskaya@gmail.com>
2022-12-13 22:31:03 +0700

    #46

--- app/src/main/java/com/example/androidinternnavigation/BlueFragment.kt
+++ app/src/main/java/com/example/androidinternnavigation/BlueFragment.kt
@@ -1,26 +1,12 @@
 package com.example.androidinternnavigation
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.androidinternnavigation.databinding.FragmentBlueBinding
 
-class BlueFragment : Fragment() {
-    private var _binding: FragmentBlueBinding? = null
-    private val binding get() = _binding!!
+class BlueFragment : BaseFragment<FragmentBlueBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentBlueBinding
+        get() = FragmentBlueBinding::inflate
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentBlueBinding.inflate(layoutInflater)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+    override fun navButtonSetOnClick() {}
 }
